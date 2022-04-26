9358787492ba1862219f553684a078f55dde109d
Thomas Kientz <thomas@kientz.fr>
2022-04-26 11:26:56 +0200

    first commit

---
+++ cube.js
@@ -0,0 +1,17 @@
+// const PostgresDriver = require("@cubejs-backend/postgres-driver");
+
+// Server options go here: https://cube.dev/docs/config#options-reference
+module.exports = {
+  // dbType: "postgres",
+  // driverFactory: () => {
+  //   return new PostgresDriver({
+  //     database: "localDB",
+  //     host: "demo-db.cube.dev",
+  //     user: "cube",
+  //     password: "12345",
+  //   });
+  // },
+  // jwt: {
+  //   key: "SECRET",
+  // },
+};
